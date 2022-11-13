0e90a0fc6d6261c9438c03aa09056bd579049a01
Superrr-VPN <superrr-vpn@gmail.com>
2022-11-13 20:49:45 +0800

    chore: add github action

---
+++ .github/post_auto_commit.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${DIR}/..
+
+# see https://docs.github.com/en/actions/learn-github-actions/environment-variables
+if [[ "${GITHUB_ACTIONS}" ]] ; then
+  git config --global user.email "actions@github.com"
+  git config --global user.name "Github Actions"
+fi
+
+if [[ $(git status --porcelain) ]]; then
+  git add .
+  git commit -m "build: skip ci"
+  git push origin master
+else
+  echo "no changes"
+fi
